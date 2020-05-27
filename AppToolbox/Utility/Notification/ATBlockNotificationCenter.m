3f20d0db3f6fd38ce27cb598d724c4444a96cd06
linzhiman <linzhiman@yy.com>
2020-05-27 18:09:23 +0800

    BlockNotificationCenter改用NSLock

--- AppToolbox/Utility/Notification/ATBlockNotificationCenter.m
+++ AppToolbox/Utility/Notification/ATBlockNotificationCenter.m
@@ -19,6 +19,8 @@
 
 @interface ATBlockNotificationCenter()
 
+@property (nonatomic, strong) NSLock *lock;
+
 @property (nonatomic, strong) NSMutableDictionary<NSString *, NSMutableArray *> *observers;//name->[WrapObj]
 @property (nonatomic, strong) NSMutableDictionary<NSString *, NSMutableSet *> *notifications;//ObjKey->[name]
 
@@ -35,6 +37,8 @@ AT_IMPLEMENT_SINGLETON(ATBlockNotificationCenter);
 {
     self = [super init];
     if (self) {
+        _lock = [[NSLock alloc] init];
+        
         _observers = [NSMutableDictionary<NSString *, NSMutableArray *> new];
         _notifications = [NSMutableDictionary<NSString *, NSMutableSet *> new];
         
@@ -53,35 +57,36 @@ AT_IMPLEMENT_SINGLETON(ATBlockNotificationCenter);
 
 - (void)addObserver:(id)observer name:(NSString *)name block:(nonnull id)block
 {
-    @synchronized(self)
-    {
-        NSMutableArray *observers = [self.observers objectForKey:name];
-        if (observers == nil) {
-            observers = [NSMutableArray new];
-            [self.observers setObject:observers forKey:name];
-        }
-        
-        ATWeakObject *aWrapObj = [[ATWeakObject alloc] init];
-        aWrapObj.target = observer;
-        aWrapObj.extension = [block copy];
-        
-        for (NSUInteger i = 0; i < observers.count; ++i) {
-            ATWeakObject *aWrapObj = observers[i];
-            if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
-                NSAssert(NO, @"addObserver twice name %@ observer %@", name, observer);
-                return;
-            }
-        }
-        
-        [observers addObject:aWrapObj];
-        
-        NSMutableSet *notifications = [self.notifications objectForKey:aWrapObj.objectKey];
-        if (notifications == nil) {
-            notifications = [NSMutableSet new];
-            [self.notifications setObject:notifications forKey:aWrapObj.objectKey];
-        }
-        [notifications addObject:name];
+    [self.lock lock];
+    
+    NSMutableArray *observers = [self.observers objectForKey:name];
+    if (observers == nil) {
+        observers = [NSMutableArray new];
+        [self.observers setObject:observers forKey:name];
     }
+    
+    ATWeakObject *aWrapObj = [[ATWeakObject alloc] init];
+    aWrapObj.target = observer;
+    aWrapObj.extension = [block copy];
+    
+    for (NSUInteger i = 0; i < observers.count; ++i) {
+        ATWeakObject *aWrapObj = observers[i];
+        if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
+            NSAssert(NO, @"addObserver twice name %@ observer %@", name, observer);
+            return;
+        }
+    }
+    
+    [observers addObject:aWrapObj];
+    
+    NSMutableSet *notifications = [self.notifications objectForKey:aWrapObj.objectKey];
+    if (notifications == nil) {
+        notifications = [NSMutableSet new];
+        [self.notifications setObject:notifications forKey:aWrapObj.objectKey];
+    }
+    [notifications addObject:name];
+    
+    [self.lock unlock];
 }
 
 - (id)forceAddObserver:(id)observer name:(NSString *)name block:(id)block
@@ -93,79 +98,82 @@ AT_IMPLEMENT_SINGLETON(ATBlockNotificationCenter);
 
 - (void)removeObserver:(id)observer name:(NSString *)name
 {
-    @synchronized(self)
-    {
-        NSMutableArray *observers = [self.observers objectForKey:name];
-        if (observers != nil) {
-            for (NSUInteger i = 0; i < observers.count; ++i) {
-                ATWeakObject *aWrapObj = observers[i];
-                if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
-                    [observers removeObjectAtIndex:i];
-                    break;
-                }
-            }
-            if (observers.count == 0) {
-                [self.observers removeObjectForKey:name];
+    [self.lock lock];
+    
+    NSMutableArray *observers = [self.observers objectForKey:name];
+    if (observers != nil) {
+        for (NSUInteger i = 0; i < observers.count; ++i) {
+            ATWeakObject *aWrapObj = observers[i];
+            if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
+                [observers removeObjectAtIndex:i];
+                break;
             }
         }
-        
-        NSString *objectKey = [ATWeakObject objectKey:observer];
-        NSMutableSet *notifications = [self.notifications objectForKey:objectKey];
-        if (notifications != nil) {
-            [notifications removeObject:name];
-            if (notifications.count == 0) {
-                [self.notifications removeObjectForKey:objectKey];
-            }
+        if (observers.count == 0) {
+            [self.observers removeObjectForKey:name];
         }
     }
+    
+    NSString *objectKey = [ATWeakObject objectKey:observer];
+    NSMutableSet *notifications = [self.notifications objectForKey:objectKey];
+    if (notifications != nil) {
+        [notifications removeObject:name];
+        if (notifications.count == 0) {
+            [self.notifications removeObjectForKey:objectKey];
+        }
+    }
+    
+    [self.lock unlock];
 }
 
 - (void)removeObserver:(id)observer
 {
-    @synchronized(self)
-    {
-        NSString *objectKey = [ATWeakObject objectKey:observer];
-        NSMutableSet *notifications = [self.notifications objectForKey:objectKey];
-        [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
-            NSString *name = obj;
-            [self removeObserver:observer name:name];
-        }];
-    }
+    [self.lock lock];
+    
+    NSString *objectKey = [ATWeakObject objectKey:observer];
+    NSMutableSet *notifications = [self.notifications objectForKey:objectKey];
+    [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSString *name = obj;
+        [self removeObserver:observer name:name];
+    }];
+    
+    [self.lock unlock];
 }
 
 - (NSArray *)blocksNamed:(NSString *)name
 {
     NSMutableArray *callbacks = [NSMutableArray new];
     
-    @synchronized(self)
-    {
-        NSMutableArray *observers = [self.observers objectForKey:name];
-        if (observers != nil) {
-            NSMutableArray *invalidatedObservers = [NSMutableArray new];
-            
-            for (ATWeakObject *aWrapObj in observers) {
-                if (aWrapObj.target != nil) {
-                    [callbacks addObject:aWrapObj.extension];
-                }
-                else {
-                    [invalidatedObservers addObject:aWrapObj];
-                }
+    [self.lock lock];
+    
+    NSMutableArray *observers = [self.observers objectForKey:name];
+    if (observers != nil) {
+        NSMutableArray *invalidatedObservers = [NSMutableArray new];
+        
+        for (ATWeakObject *aWrapObj in observers) {
+            if (aWrapObj.target != nil) {
+                [callbacks addObject:aWrapObj.extension];
             }
-            
-            for (ATWeakObject *aWrapObj in invalidatedObservers) {
-                NSString *objectKey = aWrapObj.objectKey;
-                NSMutableSet *notifications = [self.notifications objectForKey:objectKey];
-                [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
-                    NSString *name = obj;
-                    NSMutableArray *array = [self.observers objectForKey:name];
-                    if (array != nil) {
-                        [array removeObject:aWrapObj];
-                    }
-                }];
-                [self.notifications removeObjectForKey:objectKey];
+            else {
+                [invalidatedObservers addObject:aWrapObj];
             }
         }
+        
+        for (ATWeakObject *aWrapObj in invalidatedObservers) {
+            NSString *objectKey = aWrapObj.objectKey;
+            NSMutableSet *notifications = [self.notifications objectForKey:objectKey];
+            [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
+                NSString *name = obj;
+                NSMutableArray *array = [self.observers objectForKey:name];
+                if (array != nil) {
+                    [array removeObject:aWrapObj];
+                }
+            }];
+            [self.notifications removeObjectForKey:objectKey];
+        }
     }
+    
+    [self.lock unlock];
     
     return callbacks;
 }
@@ -174,37 +182,38 @@ AT_IMPLEMENT_SINGLETON(ATBlockNotificationCenter);
 
 - (void)addNativeObserver:(id)observer name:(NSString *)name block:(ATBNNativeBlock)block
 {
-    @synchronized(self)
-    {
-        NSMutableArray *observers = [self.nativeObservers objectForKey:name];
-        if (observers == nil) {
-            observers = [NSMutableArray new];
-            [self.nativeObservers setObject:observers forKey:name];
-            [[NSNotificationCenter defaultCenter] removeObserver:self name:name object:nil];
-            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onNotification:) name:name object:nil];
-        }
-        
-        ATWeakObject *aWrapObj = [[ATWeakObject alloc] init];
-        aWrapObj.target = observer;
-        aWrapObj.extension = [block copy];
-        
-        for (NSUInteger i = 0; i < observers.count; ++i) {
-            ATWeakObject *aWrapObj = observers[i];
-            if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
-                NSAssert(NO, @"addNativeObserver twice name %@ observer %@", name, observer);
-                return;
-            }
-        }
-        
-        [observers addObject:aWrapObj];
-        
-        NSMutableSet *notifications = [self.nativeNotifications objectForKey:aWrapObj.objectKey];
-        if (notifications == nil) {
-            notifications = [NSMutableSet new];
-            [self.nativeNotifications setObject:notifications forKey:aWrapObj.objectKey];
-        }
-        [notifications addObject:name];
+    [self.lock lock];
+    
+    NSMutableArray *observers = [self.nativeObservers objectForKey:name];
+    if (observers == nil) {
+        observers = [NSMutableArray new];
+        [self.nativeObservers setObject:observers forKey:name];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:name object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onNotification:) name:name object:nil];
     }
+    
+    ATWeakObject *aWrapObj = [[ATWeakObject alloc] init];
+    aWrapObj.target = observer;
+    aWrapObj.extension = [block copy];
+    
+    for (NSUInteger i = 0; i < observers.count; ++i) {
+        ATWeakObject *aWrapObj = observers[i];
+        if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
+            NSAssert(NO, @"addNativeObserver twice name %@ observer %@", name, observer);
+            return;
+        }
+    }
+    
+    [observers addObject:aWrapObj];
+    
+    NSMutableSet *notifications = [self.nativeNotifications objectForKey:aWrapObj.objectKey];
+    if (notifications == nil) {
+        notifications = [NSMutableSet new];
+        [self.nativeNotifications setObject:notifications forKey:aWrapObj.objectKey];
+    }
+    [notifications addObject:name];
+    
+    [self.lock unlock];
 }
 
 - (id)forceAddNativeObserver:(id)observer name:(NSString *)name block:(ATBNNativeBlock)block
@@ -216,80 +225,83 @@ AT_IMPLEMENT_SINGLETON(ATBlockNotificationCenter);
 
 - (void)removeNativeObserver:(id)observer name:(NSString *)name
 {
-    @synchronized(self)
-    {
-        NSMutableArray *observers = [self.nativeObservers objectForKey:name];
-        if (observers != nil) {
-            for (NSUInteger i = 0; i < observers.count; ++i) {
-                ATWeakObject *aWrapObj = observers[i];
-                if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
-                    [observers removeObjectAtIndex:i];
-                    break;
-                }
-            }
-            if (observers.count == 0) {
-                [[NSNotificationCenter defaultCenter] removeObserver:observer name:name object:nil];
-                [self.nativeObservers removeObjectForKey:name];
+    [self.lock lock];
+    
+    NSMutableArray *observers = [self.nativeObservers objectForKey:name];
+    if (observers != nil) {
+        for (NSUInteger i = 0; i < observers.count; ++i) {
+            ATWeakObject *aWrapObj = observers[i];
+            if (aWrapObj.target == observer || [aWrapObj.objectKey isEqualToString:[ATWeakObject objectKey:observer]]) {
+                [observers removeObjectAtIndex:i];
+                break;
             }
         }
-        
-        NSString *objectKey = [ATWeakObject objectKey:observer];
-        NSMutableSet *notifications = [self.nativeNotifications objectForKey:objectKey];
-        if (notifications != nil) {
-            [notifications removeObject:name];
-            if (notifications.count == 0) {
-                [self.nativeNotifications removeObjectForKey:objectKey];
-            }
+        if (observers.count == 0) {
+            [[NSNotificationCenter defaultCenter] removeObserver:observer name:name object:nil];
+            [self.nativeObservers removeObjectForKey:name];
         }
     }
+    
+    NSString *objectKey = [ATWeakObject objectKey:observer];
+    NSMutableSet *notifications = [self.nativeNotifications objectForKey:objectKey];
+    if (notifications != nil) {
+        [notifications removeObject:name];
+        if (notifications.count == 0) {
+            [self.nativeNotifications removeObjectForKey:objectKey];
+        }
+    }
+    
+    [self.lock unlock];
 }
 
 - (void)removeNativeObserver:(id)observer
 {
-    @synchronized(self)
-    {
-        NSString *objectKey = [ATWeakObject objectKey:observer];
-        NSMutableSet *notifications = [self.nativeNotifications objectForKey:objectKey];
-        [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
-            NSString *name = obj;
-            [self removeNativeObserver:observer name:name];
-        }];
-    }
+    [self.lock lock];
+    
+    NSString *objectKey = [ATWeakObject objectKey:observer];
+    NSMutableSet *notifications = [self.nativeNotifications objectForKey:objectKey];
+    [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSString *name = obj;
+        [self removeNativeObserver:observer name:name];
+    }];
+    
+    [self.lock unlock];
 }
 
 - (void)onNotification:(NSNotification *)notification
 {
     NSMutableArray *callbacks = [NSMutableArray new];
     
-    @synchronized(self)
-    {
-        NSMutableArray *observers = [self.nativeObservers objectForKey:notification.name];
-        if (observers != nil) {
-            NSMutableArray *invalidatedObservers = [NSMutableArray new];
-            
-            for (ATWeakObject *aWrapObj in observers) {
-                if (aWrapObj.target != nil) {
-                    [callbacks addObject:aWrapObj.extension];
-                }
-                else {
-                    [invalidatedObservers addObject:aWrapObj];
-                }
+    [self.lock lock];
+    
+    NSMutableArray *observers = [self.nativeObservers objectForKey:notification.name];
+    if (observers != nil) {
+        NSMutableArray *invalidatedObservers = [NSMutableArray new];
+        
+        for (ATWeakObject *aWrapObj in observers) {
+            if (aWrapObj.target != nil) {
+                [callbacks addObject:aWrapObj.extension];
             }
-            
-            for (ATWeakObject *aWrapObj in invalidatedObservers) {
-                NSString *objectKey = aWrapObj.objectKey;
-                NSMutableSet *notifications = [self.nativeNotifications objectForKey:objectKey];
-                [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
-                    NSString *name = obj;
-                    NSMutableArray *array = [self.nativeObservers objectForKey:name];
-                    if (array != nil) {
-                        [array removeObject:aWrapObj];
-                    }
-                }];
-                [self.nativeNotifications removeObjectForKey:objectKey];
+            else {
+                [invalidatedObservers addObject:aWrapObj];
             }
         }
+        
+        for (ATWeakObject *aWrapObj in invalidatedObservers) {
+            NSString *objectKey = aWrapObj.objectKey;
+            NSMutableSet *notifications = [self.nativeNotifications objectForKey:objectKey];
+            [notifications enumerateObjectsUsingBlock:^(id  _Nonnull obj, BOOL * _Nonnull stop) {
+                NSString *name = obj;
+                NSMutableArray *array = [self.nativeObservers objectForKey:name];
+                if (array != nil) {
+                    [array removeObject:aWrapObj];
+                }
+            }];
+            [self.nativeNotifications removeObjectForKey:objectKey];
+        }
     }
+    
+    [self.lock unlock];
     
     for (ATBNNativeBlock callback in callbacks) {
         AT_SAFETY_CALL_BLOCK(callback, notification.userInfo);
