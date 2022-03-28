64f720a7dfc07e80f2294236c45ac67c22be93b0
linzhiman <linzhiman@joyy.com>
2022-03-28 18:59:48 +0800

    添加waterfalllayout

---
+++ AppToolbox/UI/ATCollectionViewWaterfallLayout.h
@@ -0,0 +1,17 @@
+//
+//  ATCollectionViewWaterfallLayout.h
+//  AppToolbox
+//
+//  Created by linzhiman on 2022/3/28.
+//  Copyright © 2022 AppToolbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ATCollectionViewWaterfallLayout : UICollectionViewLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AppToolbox/UI/ATCollectionViewWaterfallLayout.m
@@ -0,0 +1,13 @@
+//
+//  ATCollectionViewWaterfallLayout.m
+//  AppToolbox
+//
+//  Created by linzhiman on 2022/3/28.
+//  Copyright © 2022 AppToolbox. All rights reserved.
+//
+
+#import "ATCollectionViewWaterfallLayout.h"
+
+@implementation ATCollectionViewWaterfallLayout
+
+@end
